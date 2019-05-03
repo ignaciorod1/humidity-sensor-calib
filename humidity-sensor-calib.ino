e04ef58700fb2a3d2d978deb2381d2a03b3b4d63
ignaciorod1 <ignacio.rodriguez.naranjo@alumnos.upm.es>
2019-05-03 17:04:45 +0200

    ranges

--- humidity-sensor-calib.ino
+++ humidity-sensor-calib.ino
@@ -5,9 +5,10 @@ int s2 = 1;
 int s3 = 2;
 
 int val[2];
+int temp = 0;
 
 void setup(){
-	Serial.begin(1000000);
+	Serial.begin(9600);
 }
 
 void loop(){
@@ -20,9 +21,18 @@ void loop(){
 			Serial.print("S");
 			Serial.print(i);
 			Serial.print(": ");
-			
-			Serial.print(val[i]);
-			Serial.print("   ");
+			switch(i){
+        case 0: 
+          Serial.print(map(val[i], 310, 670, 100, 0));
+          break;        
+        case 1: 
+          Serial.print(map(val[i], 320, 605, 100, 0));
+          break;
+        case 2:
+          Serial.print(map(val[i], 310, 700, 100, 0));
+          break;
+			}
+			Serial.print("%   ");
 		}
 
 		Serial.println(" ");
